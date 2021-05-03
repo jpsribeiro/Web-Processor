c34b896edeaeaf107e695115754155989c41859d
João Pedro Silva Ribeiro <joao.ribeiro@ecosistemas.com.br>
2021-05-03 11:26:24 -0300

    Web Processor

---
+++ WebProcessor/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+
+public class Form1
+{
+    private string GeneratedSource { get; set; }
+    private string URL { get; set; }
+
+    public string GetGeneratedHTML(string url)
+    {
+        URL = url;
+
+        Thread t = new Thread(new ThreadStart(WebBrowserThread));
+        t.SetApartmentState(ApartmentState.STA);
+        t.Start();
+        t.Join();
+
+        return GeneratedSource;
+    }
+
+    private void WebBrowserThread()
+    {
+        WebBrowser wb = new WebBrowser();
+        wb.Navigate(URL);
+
+        wb.DocumentCompleted +=
+            new WebBrowserDocumentCompletedEventHandler(
+                wb_DocumentCompleted);
+
+        while (wb.ReadyState != WebBrowserReadyState.Complete)
+            Application.DoEvents();
+
+        //Added this line, because the final HTML takes a while to show up
+        GeneratedSource = wb.Document.Body.InnerHtml;
+
+        wb.Dispose();
+    }
+
+    private void wb_DocumentCompleted(object sender,
+        WebBrowserDocumentCompletedEventArgs e)
+    {
+        WebBrowser wb = (WebBrowser)sender;
+        GeneratedSource = wb.Document.Body.InnerHtml;
+    }
+}
